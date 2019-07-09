b655746d99a8dd689435aa5154b61755c904e2e1
karan2607 <35913271+karan2607@users.noreply.github.com>
2019-07-09 00:23:19 -0500

    Create problem5.py

---
+++ problem5.py
@@ -0,0 +1,8 @@
+try:
+    x = int(input("Please provide an int value"))
+    if x>9 and x<51:
+        print("Yes I am in the range")
+    else:
+        print("Oops")
+except:
+    print("Please proide an int value")  
